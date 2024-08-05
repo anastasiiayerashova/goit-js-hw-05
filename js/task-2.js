7f36f0eeaa37e7c26c6bd30c0fef2db01d458d5f
Anastasiia Yerashova <yerashova.a@gmail.com>
2024-08-05 19:04:52 +0200

    tasks done

---
+++ js/task-2.js
@@ -0,0 +1,76 @@
+const getUsersWithFriend = (users, friendName) =>
+    users.filter(user => user.friends.includes(friendName));
+
+
+// const getUsersWithFriend = (users, friendName) => {
+//     return users.filter(user => user.friends.includes(friendName));
+// }
+
+
+// const getUsersWithFriend = (users, friendName) => {
+//     const result = [];
+//     users.forEach(user => {
+//         if (user.friends.includes(friendName)) {
+//             result.push(user);
+//         }
+//     });
+//     return result;
+// }
+
+
+const allUsers = [
+  {
+    name: "Moore Hensley",
+    friends: ["Sharron Pace"]
+  },
+  {
+    name: "Sharlene Bush",
+    friends: ["Briana Decker", "Sharron Pace"]
+  },
+  {
+    name: "Ross Vazquez",
+    friends: ["Marilyn Mcintosh", "Padilla Garrison", "Naomi Buckner"]
+  },
+  {
+    name: "Elma Head",
+    friends: ["Goldie Gentry", "Aisha Tran"]
+  },
+  {
+    name: "Carey Barr",
+    friends: ["Jordan Sampson", "Eddie Strong"]
+  },
+  {
+    name: "Blackburn Dotson",
+    friends: ["Jacklyn Lucas", "Linda Chapman"]
+  },
+  {
+    name: "Sheree Anthony",
+    friends: ["Goldie Gentry", "Briana Decker"]
+  }
+];
+
+console.log(getUsersWithFriend(allUsers, "Briana Decker")); 
+// [
+//   {
+//     name: "Sharlene Bush",
+//     friends: ["Briana Decker", "Sharron Pace"]
+//   },
+//   {
+//     name: "Sheree Anthony",
+//     friends: ["Goldie Gentry", "Briana Decker"]
+//   }
+// ]
+
+console.log(getUsersWithFriend(allUsers, "Goldie Gentry"));
+// [
+//   {
+//     name: "Elma Head",
+//     friends: ["Goldie Gentry", "Aisha Tran"]
+//   },
+//   {
+//     name: "Sheree Anthony",
+//     friends: ["Goldie Gentry", "Briana Decker"]
+//   }
+// ]
+
+console.log(getUsersWithFriend(allUsers, "Adrian Cross" )); // []
